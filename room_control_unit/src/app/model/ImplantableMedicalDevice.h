c960bda723e019029e71e67299c8f99a6d363b72
Andrea Giulianelli <andrygiulio@gmail.com>
2023-01-30 12:39:52 +0100

    chore: add patient and implantable medical device model

---
+++ room_control_unit/src/app/model/ImplantableMedicalDevice.h
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2023 Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+#ifndef __IMPLANTABLE_MEDICAL_DEVICE__
+#define __IMPLANTABLE_MEDICAL_DEVICE__
+
+/*
+    This enum models the type of an implantable medical device.
+*/
+enum class DeviceType {
+    /*
+        Pace maker.
+    */
+    PACE_MAKER
+};
+
+/*
+    This class model the necessary information about an implantable medical device.
+*/
+class ImplantableMedicalDevice {
+    public:
+        /*
+            Constructor.
+
+            @param id the id of the implantable medical device.
+            @param type the type of the implantable medical device.
+        */
+        ImplantableMedicalDevice(const String id, const DeviceType type): id(id), type(type) {}
+
+        /*
+            Get the Implantable medical device's ID.
+
+            @return the id.
+        */
+        String getId() {
+            return this->id;
+        }
+
+        /*
+            Get the Implantable medical device's type.
+
+            @return the type.
+        */
+        DeviceType getType() {
+            return this->type;
+        }
+    
+    private:
+        const String id;
+        const DeviceType type;
+};
+
+#endif
---
+++ room_control_unit/src/app/model/Person.h
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2023 Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+#ifndef __PERSON__
+#define __PERSON__
+
+/*
+    This enum models the role of a Person.
+*/
+enum class PersonRole {
+    /*
+        Health Professional inside the hospital.
+    */
+    HEALTH_PROFESSIONAL,
+
+    /*
+        Patient.
+    */
+    PATIENT
+};
+
+/*
+    This class model the necessary information about a Person.
+*/
+class Person {
+    public:
+        /*
+            Constructor of a Person.
+
+            @param id the person id.
+            @param role the person role.
+        */
+        Person(const String id, const PersonRole role): id(id), role(role) {}
+        
+        /*
+            Get the Person's ID.
+
+            @return the id.
+        */
+        String getId() {
+            return this->id;
+        }
+
+        /*
+            Get the Person's Role.
+
+            @return the role.
+        */
+        PersonRole getRole() {
+            return this->role;
+        }
+
+    private:
+        const String id;
+        const PersonRole role;
+};
+
+#endif

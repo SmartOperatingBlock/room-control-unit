15748f4ddf5d219310090a87b6b5107b595addfd
Andrea Giulianelli <andrygiulio@gmail.com>
2023-01-30 08:48:12 +0100

    chore: simple example of functioning

--- room_control_unit/room_control_unit.ino
+++ room_control_unit/room_control_unit.ino
@@ -6,10 +6,27 @@
  * https://opensource.org/licenses/MIT.
  */
 
+#include "src/system/System.h"
+#include "src/system/task/AbstractTask.h"
+
+class MyTask: public AbstractTask {
+    public:
+        MyTask(int period, String name) : AbstractTask(period), name(name) {}
+        void tick() {
+            Serial.println("[" + name + "@" + String(millis()) + "]computing...");
+        }
+
+    private:
+        String name;
+};
+
 void setup() {
-  
+    Serial.begin(9600);
+    System::getInstance()->addTask(new MyTask(1000, "Task-1"));
+    System::getInstance()->addTask(new MyTask(500, "Task-2"));
+    System::getInstance()->addTask(new MyTask(300, "Task-3"));
 }
 
 void loop() {
-  
+    System::getInstance()->run();
 }

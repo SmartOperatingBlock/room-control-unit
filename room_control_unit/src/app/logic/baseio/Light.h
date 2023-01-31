2ff75bfab44cfbb93efbbc1a073eecd64b79b190
Andrea Giulianelli <andrygiulio@gmail.com>
2023-01-31 16:59:47 +0100

    chore: add light and surgical light concepts

---
+++ room_control_unit/src/app/logic/baseio/Light.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2023 Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+#ifndef __LIGHT__
+#define __LIGHT__
+
+
+/*
+    Interface that models a light.
+*/
+class Light {
+    /*
+        Turn the light on with an intensity.
+
+        @param intensityPercentage the percentage of intensity with which turn on the light.
+    */
+    virtual void turnOn(int intensityPercentage) = 0;
+
+    /*
+        Turn the light off.
+    */
+    virtual void turnOff() = 0;
+};
+
+#endif
---
+++ room_control_unit/src/app/logic/baseio/SurgicalLight.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2023 Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+#ifndef __SURGICAL_LIGHT__
+#define __SURGICAL_LIGHT__
+
+#include "Light.h"
+
+/*
+    Interface that models a Surgical Light.
+*/
+class SurgicalLight: public Light {};
+
+#endif

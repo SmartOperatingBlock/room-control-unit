eb1373857443c68bf70a93ec3a5894ade415445c
Andrea Giulianelli <andrygiulio@gmail.com>
2023-02-03 12:28:44 +0100

    chore: add pin list

---
+++ room_control_unit/src/config/PinList.h
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2023 Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+#ifndef __PIN_LIST__
+#define __PIN_LIST__
+
+#define TEMPERATURE_HUMIDITY_OR 13
+#define TEMPERATURE_HUMIDITY_PRE 12
+
+#define OPERATING_TABLE_PRESSURE_SENSOR 11
+
+#define PRESENCE_SENSOR_OR 10
+#define PRESENCE_SENSOR_PRE 9
+
+#define SURGICAL_LIGHT 8
+
+#define AMBIENT_LIGHT_OR 7
+#define AMBIENT_LIGHT_PRE 6
+
+#define VENTILATION_PRE 5
+#define VENTILATION_OR 4
+
+#define HEATING_OR 22
+#define COOLING_OR 24
+
+#define HEATING_PRE 26
+#define COOLING_PRE 28
+
+#define ENTRANCE_READER_PRE 30
+#define ENTRANCE_READER_OR 32
+#define IMPLANTABLE_MEDICAL_DEVICE_BIN 34
+
+#define READER_RESET_PIN 49
+
+#define LUMINOSITY_SENSOR_PRE A0
+#define LUMINOSITY_SENSOR_OR A1
+
+#endif

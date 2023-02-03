e00dd9d255ac1acbeec07897a51f68367e527cce
Andrea Giulianelli <andrygiulio@gmail.com>
2023-02-03 12:29:00 +0100

    chore: add fsm periods

---
+++ room_control_unit/src/config/FsmPeriod.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2023 Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+#ifndef __FSM_PERIOD__
+#define __FSM_PERIOD__
+
+#define COMMAND_LISTENER_PERIOD 500
+#define TEMPERATURE_HUMIDITY_PERIOD 5000
+#define LUMINOSITY_PERIOD 5000
+#define PRESENCE_MONITORING 500
+#define PEOPLE_TRACKING_PERIOD 500
+#define OR_ADVANCE_MONITORING 500
+#define AMBIENT_LIGHT 500
+#define HVAC_SYSTEM 500
+#define OR_SURGICAL_LIGHT 500
+#define GATEWAY_EXPORTER 500
+
+#endif

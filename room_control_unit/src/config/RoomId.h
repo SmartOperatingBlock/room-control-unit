b60699ed9381946dca0021d54beb498eae807e25
Andrea Giulianelli <andrygiulio@gmail.com>
2023-02-03 12:42:58 +0100

    chore: add room id configuration

---
+++ room_control_unit/src/config/RoomId.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2023 Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+#ifndef __ROOM_ID__
+#define __ROOM_ID__
+
+
+#define OPERATING_ROOM_ID "#1"
+#define PRE_OPERATING_ROOM_ID "#2"
+
+#endif

731ed790eb1e606031a1762be352ba8a82b30347
Andrea Giulianelli <andrygiulio@gmail.com>
2023-02-03 09:52:05 +0100

    chore: add json command serializer

---
+++ room_control_unit/src/app/logic/presenter/deserializer/json/JsonCommandDeserializer.cpp
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2023 Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+#include "JsonCommandDeserializer.h"
+#include "ArduinoJson.h"
+
+#define BASE_DESERIALIZATION_SIZE 64
+
+JsonCommandDeserializer::JsonCommandDeserializer() {};
+
+Command* JsonCommandDeserializer::deserialize(const String rawCommand) {
+    DynamicJsonDocument doc(BASE_DESERIALIZATION_SIZE);
+    const DeserializationError error = deserializeJson(doc, rawCommand);
+    Command* result = nullptr;
+    
+    // Check if the deserialization has been successful.
+    if(!error) {
+        const int commandType = doc["command"];
+        switch(commandType) {
+            case static_cast<int>(CommandType::HEATING): {
+                    const String roomId = doc["room_id"];
+                    const PowerStatus statusToSet = statusFromString(doc["on"]);
+                    result = new HeatingCommand(Room(roomId), statusToSet);
+                break;
+            }
+            case static_cast<int>(CommandType::COOLING): {
+                    const String roomId = doc["room_id"];
+                    const PowerStatus statusToSet = statusFromString(doc["on"]);
+                    result = new CoolingCommand(Room(roomId), statusToSet);
+                break;
+            }
+            case static_cast<int>(CommandType::VENTILATION): {
+                    const String roomId = doc["room_id"];
+                    const int intensityPercentage = doc["intensity"];
+                    result = new VentilationCommand(Room(roomId), Percentage(intensityPercentage));
+                break;
+            }
+            case static_cast<int>(CommandType::AMBIENT_LIGHT): {
+                    const String roomId = doc["room_id"];
+                    const int intensityPercentage = doc["intensity"];
+                    result = new AmbientLigthCommand(Room(roomId), Percentage(intensityPercentage));
+                break;
+            }
+            case static_cast<int>(CommandType::SURGICAL_LIGHT): {
+                    const String roomId = doc["room_id"];
+                    const int intensityPercentage = doc["intensity"];
+                    result = new SurgicalLightCommand(Room(roomId), Percentage(intensityPercentage));
+                break;
+            }
+            default:
+                break;
+        }
+    }
+
+    return result;
+}
+
+PowerStatus JsonCommandDeserializer::statusFromString(const String rawStatus) {
+    return rawStatus == "on" ? PowerStatus::ON : PowerStatus::OFF;
+}
---
+++ room_control_unit/src/app/logic/presenter/deserializer/json/JsonCommandDeserializer.h
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2023 Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+#ifndef __JSON_COMMAND_DESERIALIZER__
+#define __JSON_COMMAND_DESERIALIZER__
+
+#include "../CommandDeserializer.h"
+
+/*
+    Class that implement the deserialization of commands sent in the JSON format.
+*/
+class JsonCommandDeserializer: public CommandDeserializer {
+    public:
+        /*
+            Constructor.
+        */
+        JsonCommandDeserializer();
+
+        Command* deserialize(String rawCommand);
+
+    private:
+        /*
+            Convert string in PowerStatus enum.
+
+            @param rawStatus the status expressed as a String.
+            @return the PowerStatus enum.
+        */
+        PowerStatus statusFromString(String rawStatus);
+};
+
+#endif

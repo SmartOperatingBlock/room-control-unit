f7d0313a719fde6aa72d6647fc2a87870c41a785
Andrea Giulianelli <andrygiulio@gmail.com>
2023-02-04 14:05:37 +0100

    chore: add operating room advanced monitoring task to the system

--- room_control_unit/room_control_unit.ino
+++ room_control_unit/room_control_unit.ino
@@ -23,6 +23,7 @@ void setup() {
     System::getInstance()->addTask(new LuminosityMonitoring(LUMINOSITY_PERIOD, getLuminosityMonitoringContext(eventList)));
     System::getInstance()->addTask(new PresenceMonitoring(PRESENCE_MONITORING_PERIOD, getPresenceMonitoringContext(eventList)));
     System::getInstance()->addTask(new PeopleTracking(PEOPLE_TRACKING_PERIOD, getPeopleTrackingContext(eventList)));
+    System::getInstance()->addTask(new ORAdvancedMonitoring(OR_ADVANCE_MONITORING_PERIOD, getORAdvancedMonitoringContext(eventList)));
 }
 
 void loop() {

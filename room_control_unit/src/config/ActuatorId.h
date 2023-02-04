a500cc6f32e7ac25bd41831a06a1dedd5fa30faf
Andrea Giulianelli <andrygiulio@gmail.com>
2023-02-04 17:29:07 +0100

    chore: add actuator id list

---
+++ room_control_unit/src/config/ActuatorId.h
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2023 Smart Operating Block
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
+#ifndef __ACTUATOR_ID__
+#define __ACTUATOR_ID__
+
+#define VENTILATION_OR_ID "vor-324"
+#define VENTILATION_PRE_ID "vpre-333"
+
+#define HEATING_OR_ID "hor-324"
+#define HEATING_PRE_ID "hpre-333"
+
+#define COOLING_OR_ID "cor-324"
+#define COOLING_PRE_ID "cpre-333"
+
+#define AMBIENT_LIGHT_OR_ID "al-324"
+#define AMBIENT_LIGHT_PRE_ID "al-333"
+
+#define SURGICAL_LIGHT_ID "sl-342"
+
+#endif

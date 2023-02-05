0de8d98c759b6375f9e86f4d413d3cd63ecee0de
Andrea Giulianelli <andrygiulio@gmail.com>
2023-02-05 17:04:03 +0100

    refactor: refactor init code

--- room_control_unit/room_control_unit.ino
+++ room_control_unit/room_control_unit.ino
@@ -12,28 +12,36 @@
 #include "src/app/model/event/Event.h"
 #include "src/config/ContextProvider.h"
 #include "src/app/io/communication/SerialInterface.h"
+#include "src/utils/ArrayStream.h"
 
 #define SERIAL_INTERFACE_BAUD_RATE 115200
+#define TASK_NUMBER 12
 
 void setup() {
-    System::getInstance()->init();
     List<Event*>* const eventList = new List<Event*>();
     Command* startCommand = nullptr;
     Command** currentCommand = &startCommand;
-    SerialInterface::getInstance()->init(SERIAL_INTERFACE_BAUD_RATE);
     
-    System::getInstance()->addTask(new CommandListener(COMMAND_LISTENER_PERIOD, getCommandListenerContext(SerialInterface::getInstance(), currentCommand)));
-    System::getInstance()->addTask(new TemperatureHumidityMonitoring(TEMPERATURE_HUMIDITY_PERIOD, getTemperatureHumidityMonitoringContext(eventList)));
-    System::getInstance()->addTask(new LuminosityMonitoring(LUMINOSITY_PERIOD, getLuminosityMonitoringContext(eventList)));
-    System::getInstance()->addTask(new PresenceMonitoring(PRESENCE_MONITORING_PERIOD, getPresenceMonitoringContext(eventList)));
-    System::getInstance()->addTask(new PeopleTracking(PEOPLE_TRACKING_PERIOD, getPeopleTrackingContext(eventList)));
-    System::getInstance()->addTask(new ORAdvancedMonitoring(OR_ADVANCE_MONITORING_PERIOD, getORAdvancedMonitoringContext(eventList)));
-    System::getInstance()->addTask(new HvacSystem(HVAC_SYSTEM_PERIOD, getHvacSystemContextOperatingRoom(eventList, currentCommand)));
-    System::getInstance()->addTask(new HvacSystem(HVAC_SYSTEM_PERIOD, getHvacSystemContextPreOperatingRoom(eventList, currentCommand)));
-    System::getInstance()->addTask(new AmbientLightSystem(AMBIENT_LIGHT_PERIOD, getAmbientLightSystemContextOperatingRoom(eventList, currentCommand)));
-    System::getInstance()->addTask(new AmbientLightSystem(AMBIENT_LIGHT_PERIOD, getAmbientLightSystemContextPreOperatingRoom(eventList, currentCommand)));
-    System::getInstance()->addTask(new ORSurgicalLightSystem(OR_SURGICAL_LIGHT_PERIOD, getORSurgicalLightSystemContext(eventList, currentCommand)));
-    System::getInstance()->addTask(new GatewayExporter(GATEWAY_EXPORTER_PERIOD, getGatewayExporterContext(eventList, SerialInterface::getInstance())));
+    SerialInterface::getInstance()->init(SERIAL_INTERFACE_BAUD_RATE);
+    System::getInstance()->init();
+    
+    Task* taskArray[TASK_NUMBER] = {
+        new CommandListener(COMMAND_LISTENER_PERIOD, getCommandListenerContext(SerialInterface::getInstance(), currentCommand)),
+        new TemperatureHumidityMonitoring(TEMPERATURE_HUMIDITY_PERIOD, getTemperatureHumidityMonitoringContext(eventList)),
+        new LuminosityMonitoring(LUMINOSITY_PERIOD, getLuminosityMonitoringContext(eventList)),
+        new PresenceMonitoring(PRESENCE_MONITORING_PERIOD, getPresenceMonitoringContext(eventList)),
+        new PeopleTracking(PEOPLE_TRACKING_PERIOD, getPeopleTrackingContext(eventList)),
+        new ORAdvancedMonitoring(OR_ADVANCE_MONITORING_PERIOD, getORAdvancedMonitoringContext(eventList)),
+        new HvacSystem(HVAC_SYSTEM_PERIOD, getHvacSystemContextOperatingRoom(eventList, currentCommand)),
+        new HvacSystem(HVAC_SYSTEM_PERIOD, getHvacSystemContextPreOperatingRoom(eventList, currentCommand)),
+        new AmbientLightSystem(AMBIENT_LIGHT_PERIOD, getAmbientLightSystemContextOperatingRoom(eventList, currentCommand)),
+        new AmbientLightSystem(AMBIENT_LIGHT_PERIOD, getAmbientLightSystemContextPreOperatingRoom(eventList, currentCommand)),
+        new ORSurgicalLightSystem(OR_SURGICAL_LIGHT_PERIOD, getORSurgicalLightSystemContext(eventList, currentCommand)),
+        new GatewayExporter(GATEWAY_EXPORTER_PERIOD, getGatewayExporterContext(eventList, SerialInterface::getInstance()))
+    };
+    
+    ArrayStream<Task*>(taskArray, TASK_NUMBER)
+        .foreach([](Task* const task) {System::getInstance()->addTask(task);});
 }
 
 void loop() {
